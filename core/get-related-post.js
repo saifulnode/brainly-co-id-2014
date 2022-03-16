d7c43210b49bd9bf8747d32e538031550211cdf3
adlin saiful <96131038+saifulnode@users.noreply.github.com>
2022-03-16 12:19:17 +0000

    as

--- core/get-related-post.js
+++ core/get-related-post.js
@@ -16,12 +16,17 @@ module.exports = async (category)=>{
       const countRelated = settings["related-post"].count;
       for(let i=0;i<countRelated;i++){
         const randomPost = Math.floor(Math.random() * val.length);
-        const idPost = val[randomPost];
-        const dataPost = await getDataPost(idPost);
-        console.log(dataPost);
+        const mapPost = val[randomPost];
+        const nameFile = await mapPost[0].replace(".zip","");
+        const idPost = mapPost[1];
+        const dataPost = await getDataPost(nameFile,idPost);
+        await sendBack[name].push({
+          "title" : dataPost.title,
+          "time" : dataPost.time,
+          "path-link" : "/"+settings["pathname-content"]+nameFile+settings["index-post"]+idPost
+        });
       };
     }catch(e){};
   };
-  console.log(sendBack)
-  return [];
+  return sendBack;
 };
